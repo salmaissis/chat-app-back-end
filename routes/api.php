3f5a4044b8bee1c97711effacb3f0f1fae4512b1
salmaissis <isalma.issis@gmail.com>
2023-04-15 23:52:32 +0000

    display users

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController ;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('messages', [\App\Http\Controllers\ChatController::class, 'store']);
 Route::post('signup', [\App\Http\Controllers\SignupController::class, 'store']);
 Route::post('login', [\App\Http\Controllers\LoginController::class, 'index']);
-// Route::post('user', [\App\Http\Controllers\LoginController::class, 'show']);
+Route::post('users', [\App\Http\Controllers\UserController::class, 'index']);
